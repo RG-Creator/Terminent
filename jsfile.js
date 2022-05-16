732f2d7996c5caf5118494cd54c905c78629fd74
Rohit Gupta <56196113+RG-Creator@users.noreply.github.com>
2022-05-16 23:44:46 +0530

    Add files via upload

---
+++ jsfile.js
@@ -0,0 +1,56 @@
+// COUNTDOWN TIMER
+
+let daysItem = document.querySelector("#days");
+let hoursItem = document.querySelector("#hours");
+let minItem = document.querySelector("#min");
+let secItem = document.querySelector("#sec");
+
+
+let countDown = () => {
+  let futureDate = new Date("18 may 2022");
+  let currentDate = new Date();
+  let myDate = futureDate - currentDate;
+  //console.log(myDate);
+
+  let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+
+  let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+
+  let min = Math.floor(myDate / 1000 / 60) % 60;
+
+  let sec = Math.floor(myDate / 1000) % 60;
+
+  daysItem.innerHTML = days;
+  hoursItem.innerHTML = hours;
+  minItem.innerHTML = min;
+  secItem.innerHTML = sec;
+}
+
+countDown()
+
+setInterval(countDown, 1000)
+
+// COUNDOWN TIMER END
+
+// STicky NAVBAR
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+// STICKY NAVBAR END
+
+
+
