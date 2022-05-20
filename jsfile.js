bd9277935e07679c1d7868c1c7ea1069c1c90589
Rohit Gupta <56196113+RG-Creator@users.noreply.github.com>
2022-05-20 10:13:05 +0530

    Update jsfile.js

--- jsfile.js
+++ jsfile.js
@@ -7,7 +7,7 @@ let secItem = document.querySelector("#sec");
 
 
 let countDown = () => {
-  let futureDate = new Date("18 may 2022");
+  let futureDate = new Date("28 may 2022");
   let currentDate = new Date();
   let myDate = futureDate - currentDate;
   //console.log(myDate);
